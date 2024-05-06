3134c020b254a779ae5142d7e6ea856d6016e1c3
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-05-06 06:10:41 +0300

    Updated app delegate

--- Kirala/App/AppDelegate.swift
+++ Kirala/App/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import netfox
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         app.router.start() // Start the app
+        
+        #if DEBUG
+        DispatchQueue.global().async {
+            // Arka planda yapılacak işlemler
+            NFX.sharedInstance().start()
+        }
+        #endif
+        
         return true
     }
 
