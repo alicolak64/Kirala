fddb24d292fbaaf049607c213d0272ffbbba3413
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-05-16 22:38:48 +0300

    Implemetented new string extension

--- Kirala/Helper/Extensions/UIColor+Extension.swift
+++ Kirala/Helper/Extensions/UIColor+Extension.swift
@@ -77,4 +77,14 @@ extension UIColor {
         }
     }
     
+    var hexString: String {
+        guard let components = cgColor.components, components.count >= 3 else {
+            return ""
+        }
+        let r = Float(components[0])
+        let g = Float(components[1])
+        let b = Float(components[2])
+        return String(format: "#%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
+    }
+    
 }
