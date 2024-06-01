5bcf29b578befb82602aed368b788570cf813746
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-06-01 03:25:02 +0300

    feat: Add FilterCell and FilterCellPresenter
    This commit adds the FilterCell and FilterCellPresenter classes to the project. The FilterCell is a table view cell used to display a filter option, while the FilterCellPresenter is responsible for configuring the cell's view and loading the data. These classes are essential for implementing the filter feature in the app.

---
+++ Kirala/Views/Cells/Filter/FilterCell.swift
@@ -0,0 +1,127 @@
+//
+//  Filter.swift
+//  Kirala
+//
+//  Created by Ali Çolak on 30.05.2024.
+//
+
+import UIKit
+
+
+final class FilterCell: UITableViewCell, ReusableView {
+    
+    // MARK: - Properties
+    
+    var presenter: FilterCellPresenterProtocol! {
+        didSet {
+            presenter.load()
+        }
+    }
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = ColorText.primary.dynamicColor
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var selectedItemsLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 11)
+        label.textColor = ColorText.quaternary.dynamicColor
+        label.isHidden = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var arrowImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = Symbols.chevronRight.symbol()
+        imageView.tintColor = ColorPalette.appPrimary.dynamicColor
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var topSeparator: UIView = {
+        let view = UIView()
+        view.backgroundColor = ColorPalette.lightBorder.dynamicColor
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private var nameLabelCenterYConstraint: NSLayoutConstraint!
+    private var nameLabelTopConstraint: NSLayoutConstraint!
+    private var selectedItemsLabelTopConstraint: NSLayoutConstraint!
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        nameLabel.text = nil
+        selectedItemsLabel.text = nil
+        selectedItemsLabel.isHidden = true
+        updateConstraintsForSelectedItemsLabel(isVisible: false)
+    }
+    
+    private func setupViews() {
+        
+        contentView.backgroundColor = ColorBackground.primary.dynamicColor
+        
+        contentView.addSubviews([nameLabel, selectedItemsLabel, arrowImageView, topSeparator])
+                
+        nameLabelCenterYConstraint = nameLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        nameLabelTopConstraint = nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8)
+        selectedItemsLabelTopConstraint = selectedItemsLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 4)
+        
+        NSLayoutConstraint.activate([
+            topSeparator.topAnchor.constraint(equalTo: contentView.topAnchor),
+            topSeparator.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            topSeparator.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            topSeparator.heightAnchor.constraint(equalToConstant: 0.5),
+            
+            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            nameLabelCenterYConstraint,
+            
+            arrowImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            arrowImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+    
+    private func updateConstraintsForSelectedItemsLabel(isVisible: Bool) {
+        if isVisible {
+            nameLabelCenterYConstraint.isActive = false
+            contentView.addSubview(selectedItemsLabel)
+            
+            NSLayoutConstraint.activate([
+                nameLabelTopConstraint,
+                selectedItemsLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+                selectedItemsLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -35),
+                selectedItemsLabelTopConstraint
+            ])
+        } else {
+            NSLayoutConstraint.deactivate([nameLabelTopConstraint, selectedItemsLabelTopConstraint])
+            selectedItemsLabel.removeFromSuperview()
+            nameLabelCenterYConstraint.isActive = true
+        }
+    }
+}
+
+extension FilterCell: FilterCellViewProtocol {
+    func setSelectedItemsLabel(_ selectedItemsText: String) {
+        selectedItemsLabel.text = selectedItemsText
+        selectedItemsLabel.isHidden = selectedItemsText == ""
+        updateConstraintsForSelectedItemsLabel(isVisible: selectedItemsText != "")
+    }
+    
+    func setNameLabel(_ name: String) {
+        nameLabel.text = name
+    }
+}
---
+++ Kirala/Views/Cells/Filter/FilterCellPresenter.swift
@@ -0,0 +1,60 @@
+//
+//  FilterCellPresenter.swift
+//  Kirala
+//
+//  Created by Ali Çolak on 30.05.2024.
+//
+
+import Foundation
+
+/// Arguments to configure the Filter cell.
+struct FilterCellArguments {
+    let name: String
+    let selectedItems: [String]
+}
+
+/// Protocol defining the methods for the Filter Cell Presenter.
+protocol FilterCellPresenterProtocol {
+    /// Loads the data and configures the view.
+    func load()
+}
+
+
+/// Protocol defining the methods for the Filter Cell view.
+protocol FilterCellViewProtocol: AnyObject {
+    var presenter: FilterCellPresenterProtocol! { get set }
+    func setNameLabel(_ name: String)
+    func setSelectedItemsLabel(_ selectedItemsText: String)
+}
+
+/// Presenter class for the Filter Cell.
+final class FilterCellPresenter {
+    // MARK: - Properties
+    
+    private weak var view: FilterCellViewProtocol?
+    private let arguments: FilterCellArguments
+    
+    // MARK: - Init
+    
+    /// Initializes the presenter with a view, arguments, and type.
+    /// - Parameters:
+    ///   - view: The view that conforms to `SelectCellViewProtocol`.
+    ///   - arguments: The arguments to configure the view.
+    ///   - type: The type of the category cell.
+    init(view: FilterCellViewProtocol?, arguments: FilterCellArguments) {
+        self.view = view
+        self.arguments = arguments
+    }
+}
+
+// MARK: - FilterCellPresenterProtocol
+
+extension FilterCellPresenter: FilterCellPresenterProtocol {
+    /// Loads the data and configures the view.
+    func load() {
+        view?.setNameLabel(arguments.name)
+        if !arguments.selectedItems.isEmpty {
+            view?.setSelectedItemsLabel(arguments.selectedItems.joined(separator: ", "))
+        }
+    }
+}
