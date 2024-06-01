588b0d043e905a8c513d0a3851d35fd567e0151e
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-06-01 03:28:38 +0300

    Refactor LoadingView and LoadingContracts for improved code organization and readability

--- Kirala/Views/CustomViews/Loading/LoadingContracts.swift
+++ Kirala/Views/CustomViews/Loading/LoadingContracts.swift
@@ -7,14 +7,24 @@
 
 import Foundation
 
+/// Protocol defining the methods for showing and hiding a loading view.
 protocol LoadingViewProtocol {
     // MARK: - Methods
+    
+    /// Shows the loading indicator.
     func showLoading()
+    
+    /// Hides the loading indicator.
     func hideLoading()
 }
 
+/// Enum representing the loading state.
 enum LoadingState {
     // MARK: - Cases
+    
+    /// Indicates that loading is in progress.
     case loading
+    
+    /// Indicates that loading has completed.
     case loaded
 }
