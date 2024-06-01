c371c99035fceae3c5e58072e30e67685111a8f8
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-06-01 03:30:23 +0300

    feat: Add searchableFilterOptionsDidChange notification
    This commit adds the searchableFilterOptionsDidChange notification to the project. This notification is used to notify observers when the filter options for search functionality have changed. This addition is necessary for keeping the search results up to date with the selected filter options.

---
+++ Kirala/Helper/Extensions/Notification+Center.swift
@@ -0,0 +1,12 @@
+//
+//  Notification+Center.swift
+//  Kirala
+//
+//  Created by Ali Çolak on 31.05.2024.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let searchableFilterOptionsDidChange = Notification.Name("searchableFilterOptionsDidChange")
+}
