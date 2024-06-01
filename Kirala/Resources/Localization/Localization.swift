d6b7991ac24a9ed3c82d2d35a7c25ccb8dcae634
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-06-01 03:16:23 +0300

    Added new localization strings sources

--- Kirala/Resources/Localization/Localization.swift
+++ Kirala/Resources/Localization/Localization.swift
@@ -7,28 +7,52 @@
 
 import Foundation
 
+/// Enum representing different localization tables in the app.
 enum LocalizationTable: String {
     case common = "Common"
     case tabBar = "TabBar"
     case emptyState = "EmptyState"
+    case alert = "Alert"
+    case auth = "Auth"
+    case filter = "Filter"
 }
 
+/// A struct to manage localization from different tables.
 struct Localization {
     
-    static let common = Localization(table: LocalizationTable.common)
-    static let tabBar = Localization(table: LocalizationTable.tabBar)
-    static let emptyState = Localization(table: LocalizationTable.emptyState)
+    // MARK: - Static Properties
+    
+    static let common = Localization(table: .common)
+    static let tabBar = Localization(table: .tabBar)
+    static let emptyState = Localization(table: .emptyState)
+    static let alert = Localization(table: .alert)
+    static let auth = Localization(table: .auth)
+    static let filter = Localization(table: .filter)
+    
+    // MARK: - Instance Properties
     
     private let table: LocalizationTable
+    
+    // MARK: - Initializers
     
     private init(table: LocalizationTable) {
         self.table = table
     }
     
+    // MARK: - Public Methods
+    
+    /// Returns a localized string for a given key.
+    /// - Parameter key: The key for the localized string.
+    /// - Returns: The localized string.
     func localizedString(for key: String) -> String {
         return NSLocalizedString(key, tableName: table.rawValue, bundle: Bundle.main, value: "", comment: "")
     }
     
+    /// Returns a localized string with formatted arguments.
+    /// - Parameters:
+    ///   - key: The key for the localized string.
+    ///   - arguments: The arguments to be formatted into the localized string.
+    /// - Returns: The formatted localized string.
     func localizedString(for key: String, arguments: CVarArg...) -> String {
         return String(format: localizedString(for: key), arguments: arguments)
     }
