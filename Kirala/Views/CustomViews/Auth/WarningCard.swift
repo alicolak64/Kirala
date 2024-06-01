990f344781d753e91452122184f8b6d05cd30d59
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-06-01 03:27:33 +0300

    feat: Add WarningCard custom view for displaying warning messages
    This commit adds the WarningCard custom view to the project. The WarningCard is a UIView subclass that displays warning messages to the user. It includes a message label and supports showing and hiding the warning message with optional animation. This custom view is essential for displaying warnings in the authentication flow of the app.

---
+++ Kirala/Views/CustomViews/Auth/WarningCard.swift
@@ -0,0 +1,80 @@
+//
+//  WarningCard.swift
+//  Kirala
+//
+//  Created by Ali Çolak on 25.05.2024.
+//
+
+import UIKit
+
+protocol WarningCardProtocol {
+    func showWarningMessage(message: String, animated: Bool)
+    func hideWarningMessage(animated: Bool)
+}
+
+final class WarningCard: UIView, WarningCardProtocol {
+    
+    private lazy var messageLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = Localization.auth.localizedString(for: "RESET_PASSWORD_TIP_TITLE")
+        label.font = UIFont.boldSystemFont(ofSize: 11)
+        label.textColor = ColorText.warning.color
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        makeCardStyle()
+        backgroundColor = ColorBackground.warning.dynamicColor
+        
+        addSubviews([
+            messageLabel
+        ])
+        
+        NSLayoutConstraint.activate([
+            messageLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            messageLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+        ])
+    }
+    
+    func showWarningMessage(message: String, animated: Bool) {
+        
+        if animated {
+            alpha = 0
+            isHidden = false
+            messageLabel.text = message
+            UIView.animate(withDuration: 0.5) { [weak self] in
+                self?.alpha = 1
+                
+            }
+        } else {
+            messageLabel.text = message
+            isHidden = false
+        }
+        
+    }
+    
+    func hideWarningMessage(animated: Bool) {
+        
+        if animated {
+            UIView.animate(withDuration: 0.5) { [weak self] in
+                self?.alpha = 0
+            } completion: { [weak self] _ in
+                self?.isHidden = true
+            }
+        } else {
+            isHidden = true
+        }
+        
+    }
+    
+}
