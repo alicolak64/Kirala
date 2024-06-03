50ff1b5498bd486efcc154e2f43f8efe947d2ab2
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-06-03 18:32:14 +0300

    chore: Refactor FilterViewModel to improve code organization and readability

--- Kirala/Views/CustomViews/Filter/FilterView.swift
+++ Kirala/Views/CustomViews/Filter/FilterView.swift
@@ -9,11 +9,7 @@ import UIKit
 
 final class FilterView: UIView, FilterViewProtocol {
     
-    private var viewModel: FilterViewModel {
-        didSet {
-            viewModel.view = self
-        }
-    }
+    private var viewModel: FilterViewModel
     
     private lazy var sortButton: UIButton = {
         let button = UIButton(type: .system)
@@ -61,12 +57,54 @@ final class FilterView: UIView, FilterViewProtocol {
         return view
     }()
     
+    private lazy var headerSeparator: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = ColorPalette.border.dynamicColor
+        view.isHidden = true
+        return view
+    }()
+    
+    private lazy var badgeView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = ColorPalette.appPrimary.dynamicColor
+        view.addCornerRadius(radius: 9)
+        view.isHidden = true
+        return view
+    }()
+    
+    private lazy var badgeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        label.textColor = ColorText.white.dynamicColor
+        return label
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = ColorBackground.primary.dynamicColor
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    private var headeerViewWidthConstraint: NSLayoutConstraint?
+    
+    
     init(viewModel: FilterViewModel) {
         self.viewModel = viewModel
         super.init(frame: .zero)
-        headerContainer.addSubviews([sortButton, filterButton])
-        addSubviews([headerContainer])
+        badgeView.addSubviews([badgeLabel])
+        headerContainer.addSubviews([sortButton, filterButton, badgeView, headerSeparator])
+        addSubviews([headerContainer, collectionView])
         prepareConstraints()
+        prepareCollectionView()
     }
         
     required init?(coder: NSCoder) {
@@ -75,18 +113,51 @@ final class FilterView: UIView, FilterViewProtocol {
     
     
     private func prepareConstraints() {
+        
+        headeerViewWidthConstraint = headerContainer.widthAnchor.constraint(equalToConstant: 190)
+        headeerViewWidthConstraint?.isActive = true
+        
         NSLayoutConstraint.activate([
             headerContainer.topAnchor.constraint(equalTo: topAnchor),
             headerContainer.leadingAnchor.constraint(equalTo: leadingAnchor),
-            headerContainer.trailingAnchor.constraint(equalTo: trailingAnchor),
             headerContainer.bottomAnchor.constraint(equalTo: bottomAnchor),
             
             sortButton.leadingAnchor.constraint(equalTo: headerContainer.leadingAnchor, constant: 16),
             sortButton.centerYAnchor.constraint(equalTo: headerContainer.centerYAnchor),
+            sortButton.heightAnchor.constraint(equalTo: headerContainer.heightAnchor, multiplier: 0.6),
             
-            filterButton.leadingAnchor.constraint(equalTo: sortButton.trailingAnchor, constant: 16),
+            filterButton.leadingAnchor.constraint(equalTo: sortButton.trailingAnchor, constant: 8),
             filterButton.centerYAnchor.constraint(equalTo: headerContainer.centerYAnchor),
+            filterButton.heightAnchor.constraint(equalTo: headerContainer.heightAnchor, multiplier: 0.6),
+
+            
+            badgeView.topAnchor.constraint(equalTo: filterButton.topAnchor, constant: -4),
+            badgeView.trailingAnchor.constraint(equalTo: filterButton.trailingAnchor, constant: 4),
+            badgeView.widthAnchor.constraint(equalToConstant: 18),
+            badgeView.heightAnchor.constraint(equalToConstant: 18),
+            
+            badgeLabel.centerXAnchor.constraint(equalTo: badgeView.centerXAnchor),
+            badgeLabel.centerYAnchor.constraint(equalTo: badgeView.centerYAnchor),
+            
+            headerSeparator.leadingAnchor.constraint(equalTo: filterButton.trailingAnchor, constant: 5),
+            headerSeparator.widthAnchor.constraint(equalToConstant: 1),
+            headerSeparator.heightAnchor.constraint(equalTo: filterButton.heightAnchor , multiplier: 0.9),
+            headerSeparator.centerYAnchor.constraint(equalTo: headerContainer.centerYAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: headerContainer.trailingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            
         ])
+        
+        
+    }
+    
+    func prepareCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(FilterHeaderCell.self)
     }
     
     @objc private func didTapSortButton() {
@@ -98,11 +169,143 @@ final class FilterView: UIView, FilterViewProtocol {
     }
     
     func setSortButtonTitle(_ title: String) {
-        
+        sortButton.configuration?.attributedTitle = AttributedString(title, attributes: AttributeContainer([.font: UIFont.systemFont(ofSize: 14, weight: .regular), .foregroundColor: ColorText.primary.dynamicColor]))
     }
     
     func setFilterButtonTitle(_ title: String) {
+        filterButton.configuration?.attributedTitle = AttributedString(title, attributes: AttributeContainer([.font: UIFont.systemFont(ofSize: 14, weight: .regular), .foregroundColor: ColorText.primary.dynamicColor]))
+    }
+    
+    func setBadgeCount(_ count: Int) {
+        if count > 0 {
+            badgeView.isHidden = false
+            count > 99 ? (badgeLabel.text = "99+") : (badgeLabel.text = "\(count)")
+            filterButton.bringSubviewToFront(badgeView)
+            filterButton.backgroundColor = ColorPalette.appTertiary.dynamicColor
+            filterButton.addRoundedBorder(width: 1, color: ColorPalette.appPrimary.dynamicColor)
+        } else {
+            badgeView.isHidden = true
+        }
+    }
+    
+    func removeBadge() {
+        badgeView.isHidden = true
+        filterButton.addRoundedBorder(width: 1, color: ColorPalette.border.dynamicColor)
+        filterButton.backgroundColor = ColorBackground.primary.dynamicColor
+    }
+    
+    func reloadCollectionView() {
+        collectionView.reloadData()
+    }
+    
+    func reloadItems(at indexPaths: [IndexPath]) {
+        collectionView.reloadItems(at: indexPaths)
+    }
+    
+    func reloadExpandState(indexPath: IndexPath, expandState: ExpandState) {
+        if let cell = collectionView.cellForItem(at: indexPath) as? FilterHeaderCell {
+            cell.presenter.updateExpandIcon(state: expandState)
+        }
+    }
+    
+    func closeExpandedCell(type: FilterType) {
+        viewModel.closeExpandedCell(type: type)
+    }
+    
+    func reloadFilterCell(type: FilterType) {
+        viewModel.reloadFilterCell(type: type)
+    }
+    
+    private func updateButtonsVisibility(isVisible: Bool = true) {
+        UIView.animate(withDuration: 0.3) { [weak self] in
+            guard let self = self else { return }
+            if isVisible {
+                self.setSortButtonToIconOnly()
+                self.setFilterButtonToIconOnly()
+                self.headerSeparator.isHidden = false
+                self.headeerViewWidthConstraint?.constant = 120
+            } else {
+                self.setSortButtonToFullTitle()
+                self.setFilterButtonToFullTitle()
+                self.headerSeparator.isHidden = true
+                self.headeerViewWidthConstraint?.constant = 190
+            }
+            self.layoutIfNeeded()
+        }
+    }
+
+    private func setSortButtonToIconOnly() {
+        UIView.transition(with: sortButton, duration: 0.3, options: .transitionCrossDissolve, animations: { [weak self] in
+            guard let self = self else { return }
+            self.sortButton.configuration?.attributedTitle = nil
+        }, completion: nil)
+    }
+
+    private func setFilterButtonToIconOnly() {
+        UIView.transition(with: filterButton, duration: 0.3, options: .transitionCrossDissolve, animations: { [weak self] in
+            guard let self = self else { return }
+            self.filterButton.configuration?.attributedTitle = nil
+        }, completion: nil)
+    }
+
+    private func setSortButtonToFullTitle() {
+        UIView.transition(with: sortButton, duration: 0.3, options: .transitionCrossDissolve, animations: { [weak self] in
+            guard let self = self else { return }
+            self.setSortButtonTitle(Localization.filter.localizedString(for: "SORT"))
+        }, completion: nil)
+    }
+
+    private func setFilterButtonToFullTitle() {
+        UIView.transition(with: filterButton, duration: 0.3, options: .transitionCrossDissolve, animations: { [weak self] in
+            guard let self = self else { return }
+            self.setFilterButtonTitle(Localization.filter.localizedString(for: "FILTER"))
+        }, completion: nil)
+    }
+
+
+    
+}
+
+extension FilterView: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        viewModel.numberOfSections()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        viewModel.numberOfItems(in: section)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let arguments = viewModel.cellForItem(at: indexPath) else {
+            return UICollectionViewCell()
+        }
         
+        let cell: FilterHeaderCell = collectionView.dequeueReusableCell(for: indexPath)
+        let presenter = FilterHeaderCellPresenter(view: cell, arguments: arguments)
+        cell.presenter = presenter
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        viewModel.didSelectItem(at: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        guard let arguments = viewModel.cellForItem(at: indexPath) else {
+            return CGSize(width: 0, height: 0)
+        }
+        let width = arguments.title.nsString.width(usingFont: UIFont.systemFont(ofSize: 14))
+        return CGSize(width: width + 30, height: headerContainer.frame.height * 0.6)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.x > 100 {
+            updateButtonsVisibility(isVisible: true)
+        } else {
+            updateButtonsVisibility(isVisible: false)
+        }
     }
     
 }
--- Kirala/Views/CustomViews/Filter/FilterViewModel.swift
+++ Kirala/Views/CustomViews/Filter/FilterViewModel.swift
@@ -7,12 +7,33 @@
 
 import Foundation
 
+struct FilterViewModelArguments: Equatable {
+    let title: String
+    let type: FilterType
+    let isAvaliableSelectedItems: Bool
+}
+
+struct FilterHeaderItem: Expandable {
+    let title: String
+    let type: FilterType
+    let isAvaliableSelectedItems: Bool
+    var expandedState: ExpandState
+}
+
 protocol FilterViewModelProtocol {
     var delegate: FilterViewDelegate? { get set }
     var view: FilterViewProtocol? { get set }
     func didTapSortButton()
     func didTapFilterButton()
+    func numberOfSections() -> Int
+    func numberOfItems(in section: Int) -> Int
+    func cellForItem(at indexPath: IndexPath) -> FilterHeaderCellArguments?
+    func didSelectItem(at indexPath: IndexPath)
+    func closeExpandedCell(type: FilterType)
+    func reloadFilterCell(type: FilterType)
 }
+
+
 
 final class FilterViewModel: FilterViewModelProtocol {
     
@@ -20,6 +41,7 @@ final class FilterViewModel: FilterViewModelProtocol {
     
     weak var delegate: FilterViewDelegate?
     weak var view: FilterViewProtocol?
+    private var items: [FilterType: FilterHeaderItem] = [:]
     
     // MARK: - Init
     
@@ -37,15 +59,66 @@ final class FilterViewModel: FilterViewModelProtocol {
         delegate?.didTapFilterButton()
     }
     
+    func numberOfSections() -> Int {
+        return 1
+    }
+    
+    func numberOfItems(in section: Int) -> Int {
+        delegate?.numberOfItems(in: section) ?? 0
+    }
+    
+    func cellForItem(at indexPath: IndexPath) -> FilterHeaderCellArguments? {
+        let item = delegate?.cellForItem(at: indexPath)
+        guard 
+            let title = item?.title,
+            let type = item?.type,
+            let isAvaliableSelectedItems = item?.isAvaliableSelectedItems
+        else { return nil }
+        let expandState = items[type]?.expandedState ?? .collapsed
+        items[type] = FilterHeaderItem(title: title, type: type, isAvaliableSelectedItems: isAvaliableSelectedItems, expandedState: expandState)
+        return FilterHeaderCellArguments(title: title, isAvaliableSelectedItems: isAvaliableSelectedItems, expandedState: expandState)
+    }
+    
+    func didSelectItem(at indexPath: IndexPath) {
+        guard let item = delegate?.cellForItem(at: indexPath) else { return }
+        items[item.type]?.expandedState.toggle()
+        view?.reloadExpandState(indexPath: indexPath, expandState: items[item.type]?.expandedState ?? .collapsed)
+        delegate?.didSelectItem(at: indexPath)
+    }
+    
+    func closeExpandedCell(type: FilterType) {
+        items[type]?.expandedState = .collapsed
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) { [weak view] in
+            view?.reloadExpandState(indexPath: IndexPath(row: type.rawValue, section: 0), expandState: .collapsed)
+        }
+    }
+
+    
+    func reloadFilterCell(type: FilterType) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) { [weak view] in
+            view?.reloadItems(at: [IndexPath(row: type.rawValue, section: 0)])
+        }
+    }
+    
 }
 
 protocol FilterViewDelegate: AnyObject {
     func didTapSortButton()
     func didTapFilterButton()
+    func numberOfItems(in section: Int) -> Int
+    func cellForItem(at indexPath: IndexPath) -> FilterViewModelArguments?
+    func didSelectItem(at indexPath: IndexPath)
 }
 
 protocol FilterViewProtocol: AnyObject {
     func setSortButtonTitle(_ title: String)
     func setFilterButtonTitle(_ title: String)
+    func setBadgeCount(_ count: Int)
+    func removeBadge()
+    func reloadCollectionView()
+    func reloadItems(at indexPaths: [IndexPath])
+    func reloadExpandState(indexPath: IndexPath, expandState: ExpandState)
+    func closeExpandedCell(type: FilterType)
+    func reloadFilterCell(type: FilterType)
 }
 
