4bce3daf40fd2daeb518414f51e89788eed387aa
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-06-06 03:22:51 +0300

    Added new common enum

---
+++ Kirala/Helper/Enums/LoadingState.swift
@@ -0,0 +1,13 @@
+//
+//  LoadingState.swift
+//  Kirala
+//
+//  Created by Ali Çolak on 5.06.2024.
+//
+
+import Foundation
+
+enum LoadingState {
+    case loading
+    case loaded(LoadingResult)
+}
