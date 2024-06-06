f5be72fe0e6abe6e483676a55f5451047e6982c2
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-06-06 03:24:06 +0300

    Updated localization logic

--- Kirala/Helper/Protocols/Searchable.swift
+++ Kirala/Helper/Protocols/Searchable.swift
@@ -21,7 +21,7 @@ extension Searchable where Self: UIViewController {
         let searchBar = UISearchBar()
         searchBar.searchBarStyle = .minimal
         searchBar.autocapitalizationType = .none
-        searchBar.placeholder = Localization.common.localizedString(for: "SEARCH_BAR_PLACEHOLDER")
+        searchBar.placeholder = Strings.Common.searchBarPlaceholder.localized
         
         if let searchTextField = searchBar.value(forKey: "searchField") as? UITextField {
             if let leftView = searchTextField.leftView as? UIImageView {
