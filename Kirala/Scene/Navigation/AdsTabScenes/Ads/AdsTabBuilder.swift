21c1e438df47d71d21a36eff9ce8b1a891cfce2e
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-06-13 00:19:12 +0300

    feat: Add ProductService dependency to AdsTabBuilder.swift

--- Kirala/Scene/Navigation/AdsTabScenes/Ads/AdsTabBuilder.swift
+++ Kirala/Scene/Navigation/AdsTabScenes/Ads/AdsTabBuilder.swift
@@ -12,7 +12,7 @@ final class AdsTabBuilder: NavigationBuilderProtocol {
     static func build(arguments: TabBarItemArguments) -> UINavigationController {
         let navigationController = UINavigationController()
         let router = AdsRouter(navigationController: navigationController)
-        let dependencies = app.resolveDependencyArray(dependencies: [.authService])
+        let dependencies = app.resolveDependencyArray(dependencies: [.authService, .productService])
         let viewModel = AdsViewModel(router: router, dependencies: dependencies)
         let viewController = AdsViewController(viewModel: viewModel)
         viewController.tabBarItem = UITabBarItem(title: arguments.title, image: arguments.image.symbol(), tag: arguments.tag)
