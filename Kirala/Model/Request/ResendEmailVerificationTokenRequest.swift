b39d03ca4fd7e412ce27c6746d4a4667092ca487
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-06-13 01:29:45 +0300

    feat: Add ResendEmailVerificationTokenRequest struct for handling email verification token resend requests

---
+++ Kirala/Model/Request/ResendEmailVerificationTokenRequest.swift
@@ -0,0 +1,12 @@
+//
+//  ResendEmailVerificationTokenRequest.swift
+//  Kirala
+//
+//  Created by Ali Çolak on 13.06.2024.
+//
+
+import Foundation
+
+struct ResendEmailVerificationTokenRequest: Codable {
+    let email: String
+}
