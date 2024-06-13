6441184795967db59313e5bb86560ba1b0d24bba
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-06-13 13:23:06 +0300

    feat: Add CampaignResponse.swift for handling campaign response data

---
+++ Kirala/Model/Response/CampaignResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CampaignResponse.swift
+//  Kirala
+//
+//  Created by Ali Çolak on 13.06.2024.
+//
+
+import Foundation
+
+struct CampaignResponse: Codable {
+    let id : String
+    let imageUrl : String
+}
