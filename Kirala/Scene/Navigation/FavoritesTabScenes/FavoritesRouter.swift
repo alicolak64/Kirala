9d1df5a09ccdb5dc9f20c0a21d9ba72eb38f43a7
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-06-14 03:51:36 +0300

    feat: Add FavoritesRouter.swift for navigating to favorites details, authentication, and home tab

---
+++ Kirala/Scene/Navigation/FavoritesTabScenes/FavoritesRouter.swift
@@ -0,0 +1,40 @@
+//
+//  FavoritesRouter.swift
+//  Kirala
+//
+//  Created by Ali Çolak on 14.06.2024.
+//
+
+import UIKit
+
+final class FavoritesRouter: FavoritesRouterProtocol {
+    
+    // MARK: - Properties
+    private weak var navigationController: UINavigationController?
+    
+    // MARK: - Initializers
+    init(navigationController: UINavigationController?) {
+        self.navigationController = navigationController
+    }
+    
+    // MARK: - Methods
+    func navigate(to route: FavoritesRoute) {
+        switch route {
+        case .detail(_):
+            let detailNavController = UINavigationController()
+            let detailViewController = DetailBuilder.build(rootNavigationController: navigationController, navigationController: detailNavController)
+            detailNavController.viewControllers = [detailViewController]
+            detailNavController.modalPresentationStyle = .fullScreen
+            navigationController?.present(detailNavController, animated: true, completion: nil)
+        case .auth:
+            let authNavController = UINavigationController()
+            let authViewController = AuthBuilder.build(rootViewController: navigationController, navigationController: authNavController)
+            authNavController.viewControllers = [authViewController]
+            authNavController.modalPresentationStyle = .fullScreen
+            navigationController?.present(authNavController, animated: true, completion: nil)
+        case .home:
+            navigationController?.tabBarController?.selectedIndex = TabBarItem.home.rawValue
+        }
+    }
+    
+}
