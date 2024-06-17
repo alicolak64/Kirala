e6e3ee4f77718661a726a53fde26f9ad17071443
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-06-18 01:47:44 +0300

    refactor: Update Colorable protocol to use ColorHex struct
    This commit updates the Colorable protocol in the Colorable.swift file. It replaces the separate lightHex and darkHex properties with a single hex property of type ColorHex. The ColorHex struct now contains optional properties for lightHex and darkHex, allowing for different colors in light and dark modes. This refactor improves the clarity and flexibility of the Colorable protocol.

--- Kirala/Helper/Protocols/Colorable.swift
+++ Kirala/Helper/Protocols/Colorable.swift
@@ -7,23 +7,39 @@
 
 import UIKit
 
+struct ColorHex {
+    let lightHex: String?
+    let darkHex: String?
+    
+    // Init method for different light and dark colors
+    init(light: String, dark: String) {
+        self.lightHex = light
+        self.darkHex = dark
+    }
+
+    // Init method for the same color in light and dark modes
+    init(same: String) {
+        self.lightHex = same
+        self.darkHex = same
+    }
+    
+}
+
 protocol Colorable {
     // MARK: - Properties
-    var lightHex: String { get }
-    var darkHex: String { get }
+    var hex: ColorHex { get }
     var color: UIColor { get }
     var dynamicColor: UIColor { get }
 }
 
-// MARK: - Default Implementation
 extension Colorable {
     
     var lightColor: UIColor {
-        UIColor(hex: lightHex) ?? UIColor.systemBackground
+        UIColor(hex: hex.lightHex ?? "") ?? UIColor.systemBackground
     }
     
     var darkColor: UIColor {
-        UIColor(hex: darkHex) ?? UIColor.label
+        UIColor(hex: hex.darkHex ?? "") ?? UIColor.label
     }
     
     var color: UIColor {
