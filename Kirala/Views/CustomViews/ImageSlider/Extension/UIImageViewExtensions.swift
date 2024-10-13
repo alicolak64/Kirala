9fa520875f82e8947eac628865856f670d4890a9
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-10-13 19:32:01 +0300

    refactor: Update image loading method in UIImageViewExtensions.swift

--- Kirala/Views/CustomViews/ImageSlider/Extension/UIImageViewExtensions.swift
+++ Kirala/Views/CustomViews/ImageSlider/Extension/UIImageViewExtensions.swift
@@ -10,20 +10,16 @@ enum ImageLoadingError: Error {
 
 extension UIImageView {
     func setImage(with path: String?,
-                  placeholder: UIImage? = nil,
+                  placeholder: UIImage? = Images.placeholder.image,
+                  failureImage: UIImage? = Images.error.image,
                   completion: ImageLoadingCompletion? = nil) {
         guard let unwrappedPath = path?.trimmingCharacters(in: .whitespacesAndNewlines), let url = URL(string: unwrappedPath) else {
             completion?(.failure(.noImage))
+            self.image = failureImage
             return
         }
         
-        self.kf.setImage(with: url, placeholder: placeholder, options: nil, progressBlock: nil) { result in
-            switch result {
-            case .success(let value):
-                completion?(.success(value.image))
-            case .failure(let error):
-                completion?(.failure(.internalError(error)))
-            }
-        }
+        self.setImage(with: url)
+        
     }
 }
