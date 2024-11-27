535b3de914b7a5bf53c15fe7a0cab1bf2b57b6b6
alicolak07 <alicolak@strongpilot.com>
2024-11-27 14:36:36 +0300

    refactor: Implement setCampaignImage method in UIImageView+Extensions for improved image loading with progress tracking

--- Kirala/Helper/Extensions/UIImageView+Extensions.swift
+++ Kirala/Helper/Extensions/UIImageView+Extensions.swift
@@ -53,4 +53,47 @@ extension UIImageView {
             }
         )
     }
+    
+    
+    /// Loads an image using Kingfisher with a provided URL. Shows a failure image if loading fails, and tracks download progress.
+    /// - Parameters:
+    ///   - url: The URL of the image to be loaded.
+    ///   - placeholder: The placeholder image to be shown while the image is loading (optional).
+    ///   - failureImage: The image to be shown if the loading fails (optional).
+    func setCampaignImage(with url: URL?,
+                  placeholder: UIImage? = Images.placeholder.image,
+                  failureImage: UIImage? = Images.error.image) {
+        
+        // Set placeholder image
+        //self.image = placeholder
+        
+        let randomUrl = NetworkConstants.mockCampaignUrls[Int.random(in: 0..<NetworkConstants.mockCampaignUrls.count)]
+        
+        // Load the image using Kingfisher with progress tracking
+        self.kf.setImage(
+            //with: url,
+            with: randomUrl,
+            //placeholder: placeholder,
+            placeholder: nil,
+            options: nil,
+            progressBlock: { receivedSize, totalSize in
+                let percentage = (Double(receivedSize) / Double(totalSize)) * 100
+                print("Download progress: \(Int(percentage))%")
+                
+                // You can update any progress-related UI elements here (e.g., a progress bar)
+            },
+            completionHandler: { result in
+                switch result {
+                case .success(let value):
+                    // Set the loaded image
+                    self.image = value.image
+                    print("Image successfully loaded.")
+                case .failure(let error):
+                    // Set the failure image
+                    self.image = failureImage
+                    print("Error loading image: \(error)")
+                }
+            }
+        )
+    }
 }
